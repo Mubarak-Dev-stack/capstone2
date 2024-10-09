f94dfef8fcead6b6f6c66cdd9d637e833a15de04
Lars Laade <lars.laade@usu.com>
2024-10-09 18:01:42 +0200

    style for the navigation + toggle behaviour

--- src/components/Layout/Nav/Nav.jsx
+++ src/components/Layout/Nav/Nav.jsx
@@ -1,16 +1,65 @@
-import './Nav.css'
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+
+import "./Nav.css";
 
 function Nav() {
-    return <nav className="nav">
-        <ul className="nav__list">
-            <li className="nav__item"><a className="nav__link" href="#home">Home</a></li>
-            <li className="nav__item"><a className="nav__link" href="#about">About</a></li>
-            <li className="nav__item"><a className="nav__link" href="#menu">Menu</a></li>
-            <li className="nav__item"><a className="nav__link" href="#res">Reservations</a></li>
-            <li className="nav__item"><a className="nav__link" href="#oo">Order Online</a></li>
-            <li className="nav__item"><a className="nav__link" href="#login">Login</a></li>
-        </ul>
-    </nav>
+    const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+
+    const onButtonClickHandler = () => {
+        setIsMobileNavOpen(!isMobileNavOpen);
+    };
+
+    const icon = isMobileNavOpen ? faXmark : faBars;
+
+    return (
+        <nav className="nav">
+            <button
+                className="nav__toggle"
+                type="button"
+                title="Main menu"
+                aria-label="Main menu"
+                aria-expanded={isMobileNavOpen}
+                aria-controls="nav-menu"
+                onClick={onButtonClickHandler}
+            >
+                <FontAwesomeIcon icon={icon} size="2xl" />
+            </button>
+            <ul id="nav-menu" className="nav__list" data-open={isMobileNavOpen}>
+                <li className="nav__item">
+                    <a className="nav__link active" href="/">
+                        Home
+                    </a>
+                </li>
+                <li className="nav__item">
+                    <a className="nav__link" href="#about">
+                        About
+                    </a>
+                </li>
+                <li className="nav__item">
+                    <a className="nav__link" href="#menu">
+                        Menu
+                    </a>
+                </li>
+                <li className="nav__item">
+                    <a className="nav__link" href="#res">
+                        Reservations
+                    </a>
+                </li>
+                <li className="nav__item">
+                    <a className="nav__link" href="#oo">
+                        Order Online
+                    </a>
+                </li>
+                <li className="nav__item">
+                    <a className="nav__link" href="#login">
+                        Login
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    );
 }
 
-export default Nav
+export default Nav;
