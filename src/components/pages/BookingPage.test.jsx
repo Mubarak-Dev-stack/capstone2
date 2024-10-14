2ea7234cbcdb01e89f2077c0b043d93556aacff1
Lars Laade <lars.laade@usu.com>
2024-10-14 16:54:23 +0200

    add BookingPage test

---
+++ src/components/pages/BookingPage.test.jsx
@@ -0,0 +1,17 @@
+import {
+    createInitialAvailableTimes,
+    availableTimesReducer,
+} from "./BookingPage";
+
+test("createInitialAvailableTimes and availableTimesReducer", () => {
+    const initialTimes = createInitialAvailableTimes([]);
+    expect(initialTimes).toBeDefined();
+    expect(initialTimes.length).toBeGreaterThan(0);
+
+    const reduced = availableTimesReducer(initialTimes, {
+        type: "onDateChnage",
+        date: new Date(),
+    });
+    expect(reduced).toBeDefined();
+    expect(reduced.length).toBeGreaterThan(0);
+});
