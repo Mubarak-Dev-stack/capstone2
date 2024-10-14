deb148c0eab15921e4466af7d3a4c8055942293a
Lars Laade <lars.laade@usu.com>
2024-10-14 16:30:01 +0200

    better date input, set available times from array

--- src/components/Form/Bookingform.jsx
+++ src/components/Form/Bookingform.jsx
@@ -1,51 +1,62 @@
-import * as Yup from "yup"
-import { useFormik } from "formik"
+import * as Yup from "yup";
+import { useFormik } from "formik";
+import format from "date-fns/format";
 
-import Button from "../Button/Button"
-import Section from "../Layout/Section/Section"
-import FormError from "../Form/FormError"
-import FormItem from "../Form/FormItem"
+import Button from "../Button/Button";
+import Section from "../Layout/Section/Section";
+import FormError from "../Form/FormError";
+import FormItem from "../Form/FormItem";
 
-import "../Form/Form.css"
+import "../Form/Form.css";
 
-function BookingForm({availableTimes = [], onDateChange, onFormSubmit, isLoading = false}) {
-
+function BookingForm({
+    availableTimes = [],
+    onDateChange,
+    onFormSubmit,
+    isLoading = false,
+}) {
+    const today = new Date();
     const date = new Date();
     date.setDate(date.getDate() - 1);
 
     const schema = Yup.object({
-        date: Yup.date().min(date, "Please choose a date in the future").required("Please select a date"),
+        date: Yup.date()
+            .typeError("Please enter a valid date")
+            .min(date, "Please choose a date in the future")
+            .required("Please select a date"),
         time: Yup.string().required("Please select a time"),
-        guests: Yup.number().required("Please enter the number of guests").min(1, 'Enter min one guest').max(10, 'Max 10 guests are allowed'),
+        guests: Yup.number()
+            .required("Please enter the number of guests")
+            .min(1, "Enter min one guest")
+            .max(10, "Max 10 guests are allowed"),
         occasion: Yup.string().optional(),
-    })
+    });
 
     const { getFieldProps, handleSubmit, errors, values } = useFormik({
         initialValues: {
-            date: "",
-            time: "17:00",
+            date: format(today, 'yyyy-MM-dd'),
+            time: availableTimes?.length ? availableTimes[0] : "",
             guests: "",
             occasion: "",
         },
         validationSchema: schema,
         onSubmit: (values) => {
-
-            if (typeof onFormSubmit === 'function') {
+            if (typeof onFormSubmit === "function") {
                 onFormSubmit(values);
             }
         },
-    })
+    });
 
-    const { onChange } = getFieldProps('date');
+    const { onChange } = getFieldProps("date");
     const onDateChangeHandler = (event) => {
-        if (typeof onDateChange === 'function') {
-            onChange(event)
+        if (typeof onDateChange === "function") {
+            onChange(event);
             onDateChange({
-                type: 'onDateChnage',
-                date: values.date
+                type: "onDateChnage",
+                date: values.date,
             });
         }
-    }
+    };
 
     return (
         <Section>
@@ -53,42 +64,77 @@ function BookingForm({availableTimes = [], onDateChange, onFormSubmit, isLoading
                 <h2 className="m-b-3">Reserve a Table</h2>
                 <FormItem hasError={errors.date}>
                     <label htmlFor="res-date">Choose date *</label>
-                    <input {...getFieldProps('date')} id="res-date" type="date" aria-describedby={(errors.date) ? 'res-date-error' : null} onChange={onDateChangeHandler}/>
-                    { errors.date && <FormError id="res-date-error">{errors.date}</FormError> }
+                    <input
+                        min={format(today, 'yyyy-MM-dd')}
+                        {...getFieldProps("date")}
+                        id="res-date"
+                        type="date"
+                        aria-describedby={errors.date ? "res-date-error" : null}
+                        onChange={onDateChangeHandler}
+                    />
+                    {errors.date && (
+                        <FormError id="res-date-error">{errors.date}</FormError>
+                    )}
                 </FormItem>
                 <FormItem hasError={errors.time}>
                     <label htmlFor="res-time">Choose time *</label>
-                    <select {...getFieldProps('time')} id="res-time" aria-describedby={(errors.time) ? 'res-time-error' : null}>
-                        {availableTimes.map((time) => <option key={time}>{time}</option>)}
+                    <select
+                        {...getFieldProps("time")}
+                        id="res-time"
+                        aria-describedby={errors.time ? "res-time-error" : null}
+                    >
+                        {availableTimes.map((time) => (
+                            <option key={time}>{time}</option>
+                        ))}
                     </select>
-                    { errors.time && <FormError id="res-time-error">{errors.time}</FormError> }
+                    {errors.time && (
+                        <FormError id="res-time-error">{errors.time}</FormError>
+                    )}
                 </FormItem>
                 <FormItem hasError={errors.guests}>
                     <label htmlFor="guests">Number of guests *</label>
                     <input
-                        {...getFieldProps('guests')}
+                        {...getFieldProps("guests")}
                         id="guests"
                         type="number"
                         placeholder="1 to 10 guests"
-                        aria-describedby={(errors.date) ? 'res-guests-error' : null}
+                        aria-describedby={
+                            errors.date ? "res-guests-error" : null
+                        }
                     />
-                    { errors.guests && <FormError id="res-guests-error">{errors.guests}</FormError> }
+                    {errors.guests && (
+                        <FormError id="res-guests-error">
+                            {errors.guests}
+                        </FormError>
+                    )}
                 </FormItem>
                 <FormItem hasError={errors.occasion}>
                     <label htmlFor="occasion">Occasion</label>
-                    <select {...getFieldProps('occasion')} id="occasion" aria-describedby={(errors.occasion) ? 'res-occasion-error' : null}>
+                    <select
+                        {...getFieldProps("occasion")}
+                        id="occasion"
+                        aria-describedby={
+                            errors.occasion ? "res-occasion-error" : null
+                        }
+                    >
                         <option value="">Select a occasion</option>
                         <option value="Birthday">Birthday</option>
                         <option value="Anniversary">Anniversary</option>
                     </select>
-                    { errors.occasion && <FormError id="res-occasion-error">{errors.occasion}</FormError> }
+                    {errors.occasion && (
+                        <FormError id="res-occasion-error">
+                            {errors.occasion}
+                        </FormError>
+                    )}
                 </FormItem>
                 <div className="form__action">
-                    <Button type="submit" disabled={isLoading}>Make Your reservation</Button>
+                    <Button type="submit" disabled={isLoading}>
+                        Make Your reservation
+                    </Button>
                 </div>
             </form>
         </Section>
-    )
+    );
 }
 
-export default BookingForm
+export default BookingForm;
